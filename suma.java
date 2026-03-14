b9cd713a5ba17251a5b2741f715cb982ecb5a6a9
ariel macuchapi <arielemcm@gmail.com>
2026-03-14 19:19:45 -0400

    Archivo suma.java solo suma 2 entradas

---
+++ suma.java
@@ -0,0 +1,13 @@
+
+public class suma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 12;
+		int b = 13;
+		int c=0;
+		c = a +b;
+		System.out.println("Suma: " + c);
+	}
+
+}
